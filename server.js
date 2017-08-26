757ce4a9b3faad03f9d841cf899d15cf531ea24e
neeteenkale <neeteen.kale@gmail.com>
2017-08-26 18:31:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var articleOne = {
     
 }
 
-function createTemplete (data) {
+function createTemplate (data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
