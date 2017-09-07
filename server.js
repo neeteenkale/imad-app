bbfc21eb7ddb6803daf4807be37b7373554eddc0
neeteenkale <neeteen.kale@gmail.com>
2017-09-07 10:13:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
@@ -104,4 +108,21 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
