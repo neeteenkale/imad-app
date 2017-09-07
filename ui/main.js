35ca9ea01599ac98ab5bbbe8026541ea885c6f53
neeteenkale <neeteen.kale@gmail.com>
2017-09-07 11:33:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,13 @@ element.innerHTML = 'New Value from console'
 
 // Move the image
 
+var marginLeft = 0;
+
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    ing.style.marginLeft = marginLeft + 'px';
+}
+
 var ing = document.getElementById('madi');
 ing.onclick = function() {
     ing.style.marginLeft = '100px';
