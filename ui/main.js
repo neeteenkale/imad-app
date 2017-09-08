d125789466eaab46b2009079105ae94410fcc0df
neeteenkale <neeteen.kale@gmail.com>
2017-09-08 11:52:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,26 @@
 // counter code
 
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function() {
+
+  //Create a request object
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function(){
+      if (request.readyState == XMLHttpRequest.DONE) {
+        //Take some Action
+        if (request.status == 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+      }
+    };
     
-    // make a request to counter endpoint
+    //Make the request
+    request.open('GET', 'http://neeteenkale.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
     
-    
-    // capture the response and store it in a variable
-    
-    
-    //render the varialble in a correct span
-    
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
-}
